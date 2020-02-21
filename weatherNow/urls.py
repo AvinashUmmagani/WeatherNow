9770b0982a30d2b1c9b85a4e596267208fcd279b
AvinashUmmagani <ummagani.avinash@gmail.com>
2020-02-21 16:34:15 +0530

    change in url of about

--- weatherNow/urls.py
+++ weatherNow/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name="index"),
-    path('ab',views.about,name="about"),
+    path('abo',views.about,name="about"),
 ]
     
